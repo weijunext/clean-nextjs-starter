a150d4f8f5090c63e8f6421d7d2ab6654e2c9b7c
WEI JUN <weijunext@gmail.com>
2024-03-06 12:56:00 +0800

    feat: update copyright link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
         </div>
         <div className="mb-2 flex space-x-2">
           <div>{`©${currentYear}`}</div>{" "}
-          <Link href={siteConfig.url}>{siteConfig.creator}</Link>{" "}
+          <Link href={siteConfig.twitter.creator}>{siteConfig.creator}</Link>{" "}
           <div>All rights reserved.</div>
         </div>
       </div>
