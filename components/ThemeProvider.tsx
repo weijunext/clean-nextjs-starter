d66bffd31e382aaf2a1e6a7b6f9d36854379c5a8
weijunext <weijunext@gmail.com>
2024-12-09 22:52:40 +0800

    fix: next-theme

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
