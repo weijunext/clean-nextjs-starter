c66591237831f975397a82e8febafb0732d7ab19
weijunext <weijunext@gmail.com>
2024-12-09 13:35:05 +0800

    chore: Update env variables

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://starter.weijunext.com",
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://starter.weijunext.com",
   generateRobotsTxt: true,
   sitemapSize: 7000,
 };
